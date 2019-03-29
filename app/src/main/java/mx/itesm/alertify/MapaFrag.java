ae52e4ca637510940e4eac84ad626eb1d24c34e0
adria <adrianml333@gmail.com>
2019-03-29 13:55:16 -0600

    Se muestra mapa en mapaFrag Se añadieron permisos para el mapa Se muestra Mapa con marcador fijo a la Biblioteca

--- app/src/main/java/mx/itesm/alertify/MapaFrag.java
+++ app/src/main/java/mx/itesm/alertify/MapaFrag.java
@@ -7,25 +7,77 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import mx.itesm.alertify.R;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapaFrag extends Fragment {
+public class MapaFrag extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
 
 
     public MapaFrag() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa, container, false);
+        mView = inflater.inflate(R.layout.fragment_mapa, container, false);
+        return mView;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+
+        mMapView = (MapView)mView.findViewById(R.id.mapa);
+
+        if(mMapView != null){
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(19.596813, -99.226647)).title("Biblioteca ITESM CEM").snippet("Esto es una prueba"));
+
+        CameraPosition biblio = CameraPosition.builder().target(new LatLng(19.596813, -99.226647)).zoom(16).bearing(0).tilt(45).build();
+
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(biblio));
+
+
+    }
+
 
 }
