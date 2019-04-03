3c5ed4fe6fc9e8f8cdbc589dc90b05d4df05d718
adria <adrianml333@gmail.com>
2019-04-03 14:10:18 -0600

    Mapa con compas, zoom y boton de ubicacion actual

--- app/src/main/java/mx/itesm/alertify/MapaFrag.java
+++ app/src/main/java/mx/itesm/alertify/MapaFrag.java
@@ -1,14 +1,27 @@
 package mx.itesm.alertify;
 
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
@@ -16,15 +29,25 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
 import mx.itesm.alertify.R;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapaFrag extends Fragment implements OnMapReadyCallback {
+public class MapaFrag extends Fragment implements OnMapReadyCallback, LocationListener {
 
     GoogleMap mGoogleMap;
     MapView mMapView;
@@ -38,6 +61,8 @@ public class MapaFrag extends Fragment implements OnMapReadyCallback {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
@@ -49,17 +74,18 @@ public class MapaFrag extends Fragment implements OnMapReadyCallback {
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-        super.onViewCreated(view,savedInstanceState);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        mMapView = (MapView)mView.findViewById(R.id.mapa);
+        mMapView = (MapView) mView.findViewById(R.id.mapa);
 
-        if(mMapView != null){
+        if (mMapView != null) {
             mMapView.onCreate(null);
             mMapView.onResume();
             mMapView.getMapAsync(this);
         }
     }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -68,16 +94,54 @@ public class MapaFrag extends Fragment implements OnMapReadyCallback {
 
         mGoogleMap = googleMap;
 
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        googleMap.setMyLocationEnabled(true); // false to disable
+
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        googleMap.getUiSettings().setCompassEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true); // true to enable
+
         googleMap.addMarker(new MarkerOptions().position(new LatLng(19.596813, -99.226647)).title("Biblioteca ITESM CEM").snippet("Esto es una prueba"));
-
         CameraPosition biblio = CameraPosition.builder().target(new LatLng(19.596813, -99.226647)).zoom(16).bearing(0).tilt(45).build();
-
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(biblio));
 
+        ////
 
     }
 
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
