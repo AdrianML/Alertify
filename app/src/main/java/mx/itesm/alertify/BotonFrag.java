a7bdac43f9d07478b73e191e6b37fce3dae2ce6b
adria <adrianml333@gmail.com>
2019-04-26 16:57:16 -0500

    Modificaciones a BotonFrag y se añadio layout para cuadro de dialogo para enviar alerta al presionar boton

--- app/src/main/java/mx/itesm/alertify/BotonFrag.java
+++ app/src/main/java/mx/itesm/alertify/BotonFrag.java
@@ -1,23 +1,36 @@
 package mx.itesm.alertify;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import mx.itesm.alertify.R;
 
-public class BotonFrag extends Fragment {
+public class BotonFrag extends Fragment{
+
+    private Button btnAlert;
 
     public BotonFrag() {
         // Required empty public constructor
     }
     
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_boton, container, false);
+        View v = inflater.inflate(R.layout.fragment_boton, container, false);
+        btnAlert = v.findViewById(R.id.btnAlerta);
+
+        return v;
     }
+
 }
