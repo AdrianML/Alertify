dc8d35680d4328340a73f88c925ce0c640af6f3a
Jorge <jorgelopez2906@gmail.com>
2019-04-29 12:12:02 -0500

    Reportes/Mapa

--- app/src/main/java/mx/itesm/alertify/Report.java
+++ app/src/main/java/mx/itesm/alertify/Report.java
@@ -1,16 +1,23 @@
 package mx.itesm.alertify;
 
 public class Report {
+    private int idReporte;
     private String titulo;
     private String fecha;
     private String hora;
     private String desc;
+    private double latitud;
+    private double longitud;
 
-    public Report(String titulo, String fecha, String hora, String desc){
+    public Report(int idReporte, String titulo, String fecha, String hora, String desc, double latitud, double longitud){
+        this.idReporte = idReporte;
         this.titulo = titulo;
         this.fecha = fecha;
         this.hora = hora;
         this.desc = desc;
+        this.latitud = latitud;
+        this.longitud = longitud;
+
     }
 
     public String getTitulo() {
@@ -44,4 +51,22 @@ public class Report {
     public void setDesc(String desc) {
         this.desc = desc;
     }
+
+    public int getIdReporte() {
+        return idReporte;
+    }
+
+    public void setIdReporte(int idReporte) {
+        this.idReporte = idReporte;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+
 }
--- app/src/main/java/mx/itesm/alertify/ReporteFrag.java
+++ app/src/main/java/mx/itesm/alertify/ReporteFrag.java
@@ -1,10 +1,18 @@
 package mx.itesm.alertify;
 
 
+import android.Manifest;
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +26,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import mx.itesm.alertify.R;
 
+import static android.support.v4.content.ContextCompat.getSystemService;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +39,14 @@ public class ReporteFrag extends Fragment {
     private EditText etHora;
     private EditText etDesc;
     private Button btnEnviar;
+
+    private int idReporte = 1;
+
+    private double latitude;
+    private double longitude;
+
+    //SharedPreferences Manager
+    TinyDB tinyDB = new TinyDB(getContext());
 
     public ReporteFrag() {
         // Required empty public constructor
@@ -45,9 +63,12 @@ public class ReporteFrag extends Fragment {
         etDesc = v.findViewById(R.id.etDesc);
         btnEnviar = v.findViewById(R.id.btnEnviar);
 
+        ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("aviso", "Click");
                 subirReporte();
             }
         });
@@ -55,19 +76,68 @@ public class ReporteFrag extends Fragment {
         return v;
     }
 
-    public void subirReporte(){
+    public void subirReporte() {
+        Log.i("aviso", "estoy en subirReporte");
         String titulo = etTitulo.getText().toString();
         String fecha = etFecha.getText().toString();
         String hora = etHora.getText().toString();
         String desc = etDesc.getText().toString();
 
+        LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Log.i("aviso", "Going in");
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("aviso", "if 1");
+            // TO DO
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+        }
+        Log.i("aviso", "if 2");
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(location!= null){
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+        }
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        Log.i("aviso", "salí");
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+
+
         if(!titulo.isEmpty() && !fecha.isEmpty() && !hora.isEmpty() && !desc.isEmpty()){
-            Report newReport = new Report(titulo, fecha, hora, desc);
+            Report newReport = new Report(idReporte,titulo, fecha, hora, desc, latitude, longitude);
             FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference ruta = database.getReference("Reporte/" + titulo + "/"); //Tabla
+            DatabaseReference ruta = database.getReference("Reporte/" + idReporte + "/"); //Tabla
             ruta.setValue(newReport); //Contenido
 
             limpiarPantalla();
+
         }
         else {
             Toast.makeText(getActivity(), "Favor de llenar todos los campos.", Toast.LENGTH_LONG).show();
@@ -76,6 +146,8 @@ public class ReporteFrag extends Fragment {
     }
 
     public void limpiarPantalla(){
+        tinyDB.putInt("idReporte", idReporte);
+        idReporte++;
         etTitulo.setText("");
         etFecha.setText("");
         etHora.setText("");
@@ -83,4 +155,5 @@ public class ReporteFrag extends Fragment {
 
         Toast.makeText(getActivity(), "Reporte Enviado", Toast.LENGTH_LONG).show();
     }
+
 }
